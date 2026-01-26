f63b3ea70e28db6772ef8b0e2f95a97db2702dd7
Mikky1997 <aly.mikky97@gmail.com>
2026-01-26 16:44:09 +0200

    Update PM2 config with fnm Node.js path

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,9 +5,11 @@ module.exports = {
       script: 'npm',
       args: 'start',
       cwd: '/var/www/mikkystream',
+      interpreter: '/root/.local/share/fnm/node-versions/v24.13.0/installation/bin/node',
       env: {
         NODE_ENV: 'production',
         PORT: 3000,
+        PATH: '/root/.local/share/fnm/node-versions/v24.13.0/installation/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
       },
       instances: 1,
       autorestart: true,
